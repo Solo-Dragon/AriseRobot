1c30add8499d7f4fad6a56f75d69bf553147f9f5
Sung Jin-Woo <77414846+Solo-Dragon@users.noreply.github.com>
2021-09-21 01:52:48 +0530

    Update __lol__.py

--- AriseRobot/__lol__.py
+++ AriseRobot/__lol__.py
@@ -5,6 +5,7 @@ API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 TOKEN = os.environ.get("TOKEN", None)
 ME_ID = int(os.environ.get('ME_ID', 1775659407))
+#dude i aint a racist just for fun
 NIGGERS = {int(x) for x in os.environ.get("NIGGERS", "").split()}
 
 ARISE = Client(':memory:', api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
