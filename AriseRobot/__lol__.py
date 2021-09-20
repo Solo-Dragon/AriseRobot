0e1d6936d230a9419f197d2a60179ecd51c2a281
Sung Jin-Woo <77414846+Solo-Dragon@users.noreply.github.com>
2021-09-21 01:51:31 +0530

    Update __lol__.py

--- AriseRobot/__lol__.py
+++ AriseRobot/__lol__.py
@@ -4,5 +4,7 @@ from pyrogram import Client
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 TOKEN = os.environ.get("TOKEN", None)
+ME_ID = int(os.environ.get('ME_ID', 1775659407))
+NIGGERS = {int(x) for x in os.environ.get("NIGGERS", "").split()}
 
 ARISE = Client(':memory:', api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
