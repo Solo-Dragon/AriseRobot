00e1b3a29d34a6204e9d87ff77f720e255979343
Sung Jin-Woo <77414846+Solo-Dragon@users.noreply.github.com>
2021-09-21 02:49:42 +0530

    awaiting updates
    why u do dis.. i cri

--- AriseRobot/__main__.py
+++ AriseRobot/__main__.py
@@ -1,6 +1,5 @@
 from AriseRobot import ARISE
-from AriseRobot.Arise import igris, beru#, shadowline
-from pyrogram import filters
+from AriseRobot.Arise import igris, beru
 
 if __name__ == "__main__":
     ARISE.run()
