7486d913ba8c51423b82cd9d804fe9448e88aade
Sung Jin-Woo <77414846+Solo-Dragon@users.noreply.github.com>
2021-09-21 02:37:20 +0530

    lol
    why u do this ;-0

--- AriseRobot/__main__.py
+++ AriseRobot/__main__.py
@@ -2,5 +2,5 @@ from AriseRobot import ARISE
 from AriseRobot.Arise import igris, beru, shadowline
 from pyrogram import filters
 
-if __name__ == "__hehe__":
+if __name__ == "__main__":
     ARISE.run()
