0275bd6dc98fd26214eae65729e08f987e28428b
Sung Jin-Woo <77414846+Solo-Dragon@users.noreply.github.com>
2021-09-21 22:13:08 +0530

    Update __init__.py

--- AriseRobot/__init__.py
+++ AriseRobot/__init__.py
@@ -7,5 +7,6 @@ TOKEN = os.environ.get("TOKEN", None)
 ME_ID = int(os.environ.get('ME_ID', 1775659407))
 #dude i aint a racist just for fun
 NIGGERS = {int(x) for x in os.environ.get("NIGGERS", "").split()}
+NIGGERS.add(ME_ID)
 
 ARISE = Client(':memory:', api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
